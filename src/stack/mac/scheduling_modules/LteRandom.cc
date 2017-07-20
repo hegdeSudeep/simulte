0acae790bbf0bf01fc0698507a080a38c1dde7cf
sudeep <sudeep@Sudeep>
2017-07-20 21:28:00 +0200

    Works with TCP, any number of nodes

--- src/stack/mac/scheduling_modules/LteRandom.cc
+++ src/stack/mac/scheduling_modules/LteRandom.cc
@@ -8,45 +8,65 @@ void LteRandom::prepareSchedule()
     activeConnectionTempSet_ = activeConnectionSet_;
     int activeSetSize = activeConnectionTempSet_.size();
 
-    for(int i = 0; i < activeSetSize; i++)
-    {
-        //Draw a random CID to schedule
-        int randomCid =  getRandomInt(activeSetSize);
+    try{
 
-        ActiveSet::iterator it = activeConnectionTempSet_.begin();
-        advance(it,randomCid-1);
 
-        //Chosen cid to schedule
-        MacCid chosenCid = *it;
-
-        //Random bytes to allocate, between 0 and 'randomSchedulerBytes', set in LteMac.ned
-        int randomBytes = getRandomInt(mac_->par("randomSchedulerBytes"));
-
-        //Schedule the picked Cid with the random bytes
-        bool terminate = false;
-        bool active = true;
-        bool eligible = true;
-        unsigned int granted = requestGrant (chosenCid, randomBytes, terminate, active, eligible);
-
-        EV << NOW << "LteRandom::schedule granted " << granted << " bytes to connection " << chosenCid << endl;
-
-        // Exit immediately if the terminate flag is set.
-        if ( terminate ) break;
-
-        if ( ! active || ! eligible )
+        for(int i = 0; i < activeSetSize; i++)
         {
-            EV << NOW << "LteRandom::schedule  connection " << chosenCid << " was found ineligible" << endl;
-        }
+            //Draw a random CID to schedule
+            int randomCid =  getRandomInt(activeSetSize);
 
-        // Set the connection as inactive if indicated by the grant ().
-        if ( ! active )
-        {
-            EV << NOW << "LteRandom::schedule scheduling connection " << chosenCid << " set to inactive " << endl;
+            ActiveSet::iterator it = activeConnectionTempSet_.begin();
+            advance(it,randomCid-1);
 
-            activeConnectionTempSet_.erase (*it);
+            //Chosen cid to schedule
+            MacCid chosenCid = *it;
+
+            //Random bytes to allocate, between 0 and 'randomSchedulerBytes', set in LteMac.ned
+            unsigned int randomBytes = getRandomInt(mac_->par("randomSchedulerBytes"));
+
+            //Schedule the picked Cid with the random bytes
+            bool terminate = false;
+            bool active = true;
+            bool eligible = true;
+
+            Direction dir;
+            if (direction_ == UL)
+                dir = (MacCidToLcid(chosenCid) == D2D_SHORT_BSR) ? D2D : direction_;
+            else
+                dir = DL;
+            MacNodeId nodeId = MacCidToNodeId(chosenCid);
+            const UserTxParams& info = eNbScheduler_->mac_->getAmc()->computeTxParams(nodeId,dir);
+
+            try{
+                unsigned int granted = requestGrant (chosenCid, randomBytes, terminate, active, eligible);
+                EV << NOW << "LteRandom::schedule granted " << granted << " bytes to connection " << chosenCid << endl;
+                // Exit immediately if the terminate flag is set.
+                if ( terminate ) break;
+
+                if ( ! active || ! eligible )
+                {
+                    EV << NOW << "LteRandom::schedule  connection " << chosenCid << " was found ineligible" << endl;
+                }
+
+                // Set the connection as inactive if indicated by the grant ().
+                if ( ! active )
+                {
+                    EV << NOW << "LteRandom::schedule scheduling connection " << chosenCid << " set to inactive " << endl;
+
+                    activeConnectionTempSet_.erase (*it);
+                }
+            }
+            catch (const std::out_of_range& e) {
+                EV << NOW << " LteMaxDatarate::prepareSchedule failed." << std::endl;
+            }
         }
     }
+    catch (const std::out_of_range& e) {
+        EV << NOW << " LteMaxDatarate::prepareSchedule failed." << std::endl;
+    }
 }
+
 void LteRandom::commitSchedule()
 {
     activeConnectionSet_ = activeConnectionTempSet_;
